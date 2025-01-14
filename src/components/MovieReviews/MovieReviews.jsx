49ed67bec7e654139fea8069fca92dd7aa7a876e
Vova <volodimir.komarniczky@gmail.com>
2025-01-14 20:31:13 +0000

    react hw5

--- src/components/MovieReviews/MovieReviews.jsx
+++ src/components/MovieReviews/MovieReviews.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Loader from "../Loader/Loader";
 import { fetchMovieReviews } from "../../api";
 import styles from "./MovieReviews.module.css";
 
